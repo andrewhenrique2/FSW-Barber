7042416c2c27ddb381e4391321f111758f7dd620
andrewhenrique2 <andrewloly06@gmail.com>
2024-08-16 13:02:56 -0300

    feat: ensure only avaliable time are shown on booking process

--- src/app/_components/service-items.tsx
+++ src/app/_components/service-items.tsx
@@ -1,26 +1,25 @@
 "use client"
 
-import { Barbershop, BarbershopService } from "@prisma/client"
+import { Barbershop, BarbershopService, Booking } from "@prisma/client"
 import Image from "next/image"
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
 import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
   SheetTitle,
-  SheetTrigger,
 } from "./ui/sheet"
 import { Calendar } from "./ui/calendar"
 import { ptBR } from "date-fns/locale"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Sun, CloudSun, Moon } from "lucide-react"
-import { format, set } from "date-fns"
+import { addDays, format, set } from "date-fns"
 import { useSession } from "next-auth/react"
 import { toast } from "sonner"
 import { createBooking } from "../_actions/create-booking"
+import { getBookings } from "../_actions/get-bookings"
 
 interface ServiceItemProps {
   service: BarbershopService
@@ -28,10 +27,20 @@ interface ServiceItemProps {
 }
 
 const MORNING_TIMES = ["09:00", "10:00", "11:00"]
-
 const AFTERNOON_TIMES = ["13:00", "14:00", "15:00", "16:00", "17:00", "18:00"]
-
 const EVENING_TIMES = ["19:00", "20:00"]
+
+const filterAvailableTimes = (times: string[], bookings: Booking[]) => {
+  return times.filter((time) => {
+    const [hour, minutes] = time.split(":").map(Number)
+    const hasBookingOnCurrentTime = bookings.some(
+      (booking) =>
+        booking.date.getHours() === hour &&
+        booking.date.getMinutes() === minutes,
+    )
+    return !hasBookingOnCurrentTime
+  })
+}
 
 const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
   const { data } = useSession()
@@ -39,6 +48,27 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
   const [selectedTime, setSelectedTime] = useState<string | undefined>(
     undefined,
   )
+  const [dayBookings, setDayBookings] = useState<Booking[]>([])
+  const [bookingSheetIsOpen, setBookingSheetIsOpen] = useState(false)
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (!selectedDay) return
+      const bookings = await getBookings({
+        date: selectedDay,
+        serviceId: service.id,
+      })
+      setDayBookings(bookings)
+    }
+    fetch()
+  }, [selectedDay, service.id])
+
+  const handleBookingSheetOpenChange = () => {
+    setSelectedDay(undefined)
+    setSelectedTime(undefined)
+    setDayBookings([])
+    setBookingSheetIsOpen(false)
+  }
 
   const handleDateSelect = (date: Date | undefined) => {
     setSelectedDay(date)
@@ -52,8 +82,7 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
     try {
       if (!selectedDay || !selectedTime) return
 
-      const hour = Number(selectedTime.split(":")[0])
-      const minute = Number(selectedTime.split(":")[1])
+      const [hour, minute] = selectedTime.split(":").map(Number)
       const newDate = set(selectedDay, {
         hours: hour,
         minutes: minute,
@@ -64,6 +93,7 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
         userId: (data?.user as any).id,
         date: newDate,
       })
+      handleBookingSheetOpenChange()
       toast.success("Reservado com sucesso")
     } catch (error) {
       console.log(error)
@@ -98,12 +128,17 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
               }).format(Number(service.price))}
             </p>
 
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button variant="secondary" size="sm">
-                  Reservar
-                </Button>
-              </SheetTrigger>
+            <Sheet
+              open={bookingSheetIsOpen}
+              onOpenChange={handleBookingSheetOpenChange}
+            >
+              <Button
+                variant="secondary"
+                size="sm"
+                onClick={() => setBookingSheetIsOpen(true)}
+              >
+                Reservar
+              </Button>
               <SheetContent className="px-0">
                 <SheetHeader>
                   <SheetTitle>Fazer reserva</SheetTitle>
@@ -112,96 +147,79 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
                 <div className="border-b border-solid">
                   <Calendar
                     mode="single"
+                    fromDate={addDays(new Date(), 1)}
                     locale={ptBR}
                     selected={selectedDay}
                     onSelect={handleDateSelect}
-                    styles={{
-                      head_cell: {
-                        width: "100%",
-                        textTransform: "capitalize",
-                      },
-                      cell: {
-                        width: "100%",
-                      },
-                      button: {
-                        width: "100%",
-                      },
-                      nav_button_previous: {
-                        width: "22px",
-                        height: "22px",
-                      },
-                      nav_button_next: {
-                        width: "22px",
-                        height: "22px",
-                      },
-                      caption: {
-                        textTransform: "capitalize",
-                      },
-                    }}
                   />
                 </div>
 
-                {/* MANHA */}
-
                 {selectedDay && (
                   <div className="flex flex-col gap-4 border-b border-solid p-2">
+                    {/* MANHA */}
                     <div className="flex items-center gap-2">
                       <Sun className="text-yellow-500" />
                       <span className="font-semibold">Manhã</span>
                     </div>
                     <div className="scrollbar-thin flex gap-2 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-                      {MORNING_TIMES.map((time) => (
-                        <Button
-                          onClick={() => handleTimeSelect(time)}
-                          key={time}
-                          className="rounded-full"
-                          variant={
-                            selectedTime === time ? "default" : "outline"
-                          }
-                        >
-                          {time}
-                        </Button>
-                      ))}
+                      {filterAvailableTimes(MORNING_TIMES, dayBookings).map(
+                        (time) => (
+                          <Button
+                            onClick={() => handleTimeSelect(time)}
+                            key={time}
+                            className="rounded-full"
+                            variant={
+                              selectedTime === time ? "default" : "outline"
+                            }
+                          >
+                            {time}
+                          </Button>
+                        ),
+                      )}
                     </div>
-                    {/* TARDE */}
 
+                    {/* TARDE */}
                     <div className="flex items-center gap-2">
                       <CloudSun className="text-orange-500" />
                       <span className="font-semibold">Tarde</span>
                     </div>
                     <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-                      {AFTERNOON_TIMES.map((time) => (
-                        <Button
-                          onClick={() => handleTimeSelect(time)}
-                          key={time}
-                          className="rounded-full"
-                          variant={
-                            selectedTime === time ? "default" : "outline"
-                          }
-                        >
-                          {time}
-                        </Button>
-                      ))}
+                      {filterAvailableTimes(AFTERNOON_TIMES, dayBookings).map(
+                        (time) => (
+                          <Button
+                            onClick={() => handleTimeSelect(time)}
+                            key={time}
+                            className="rounded-full"
+                            variant={
+                              selectedTime === time ? "default" : "outline"
+                            }
+                          >
+                            {time}
+                          </Button>
+                        ),
+                      )}
                     </div>
-                    {/* NOITE */}
 
+                    {/* NOITE */}
                     <div className="flex items-center gap-2">
                       <Moon className="text-blue-500" />
                       <span className="font-semibold">Noite</span>
                     </div>
                     <div className="scrollbar-thin flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-                      {EVENING_TIMES.map((time) => (
-                        <Button
-                          key={time}
-                          onClick={() => handleTimeSelect(time)}
-                          className="rounded-full"
-                          variant={
-                            selectedTime === time ? "default" : "outline"
-                          }
-                        >
-                          {time}
-                        </Button>
-                      ))}
+                      {filterAvailableTimes(EVENING_TIMES, dayBookings).map(
+                        (time) => (
+                          <Button
+                            key={time}
+                            onClick={() => handleTimeSelect(time)}
+                            className="rounded-full"
+                            variant={
+                              selectedTime === time ? "default" : "outline"
+                            }
+                          >
+                            {time}
+                          </Button>
+                        ),
+                      )}
                     </div>
                   </div>
                 )}
@@ -247,9 +265,7 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
 
                 {selectedTime && selectedDay && (
                   <SheetFooter className="px-5">
-                    <SheetClose asChild>
-                      <Button onClick={handleCreateBooking}>Confirmar</Button>
-                    </SheetClose>
+                    <Button onClick={handleCreateBooking}>Confirmar</Button>
                   </SheetFooter>
                 )}
               </SheetContent>
